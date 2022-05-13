f08e2937a8e5bd654a5bd3b729cf8ee351f2dc6d
Bazzo10 <nico-bazzo@hotmail.com>
2022-05-13 13:47:11 -0300

    make it better

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/'));//app.use(express.static(__dirname + '/dist/angular.io-example'));
+app.use(express.static(__dirname + '/dist/angular-store-example'));//app.use(express.static(__dirname + '/dist/angular.io-example'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-    '/dist/index.html'));});//'/dist/angular.io-example/index.html'));});
+    '/dist/angular-store-example/index.html'));});//'/dist/angular.io-example/index.html'));});
 app.listen(process.env.PORT || 8080);
